7bf3b0bdbc1283b40666b6b063646dae675d339b
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-02-06 11:14:24 +0100

    Add Title Test

--- src/components/Title.test.js
+++ src/components/Title.test.js
@@ -5,6 +5,10 @@ import Title from './Title'
 describe('with a different content prop', () => {
   const title = shallow(<Title content="Something Completely Different!" />)
 
+  it('has a wrapping h1 tag', () => {
+   expect(title).toHaveTagName('h1')
+ })
+
   it('renders a different title text', () => {
     expect(title).toHaveText('Something Completely Different!')
   })
